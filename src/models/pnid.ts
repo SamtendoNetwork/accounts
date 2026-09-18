ecb091a54286a340e7926ffbd2fa96953dc016c8
Sam <rdp@samtendo.net>
2026-09-18 22:49:55 +0100

    Looks good to me!

--- src/models/pnid.ts
+++ src/models/pnid.ts
@@ -6,8 +6,8 @@ import TGA from 'tga';
 import got from 'got';
 import Mii from 'mii-js';
 import Stripe from 'stripe';
-import { createChannel, createClient, Metadata } from 'nice-grpc';
-import { MiiverseServiceDefinition } from '@pretendonetwork/grpc/miiverse/v2/miiverse_service';
+// import { createChannel, createClient, Metadata } from 'nice-grpc';
+// import { MiiverseServiceDefinition } from '@pretendonetwork/grpc/miiverse/v2/miiverse_service';
 import { DeviceSchema } from '@/models/device';
 import { uploadCDNAsset } from '@/util';
 import { LOG_ERROR, LOG_WARN } from '@/logger';
@@ -28,9 +28,9 @@ if (config.stripe?.secret_key) {
 	});
 }
 
-// TODO - Make this optional later
-const gRPCMiiverseChannel = createChannel(`${config.grpc.miiverse.host}:${config.grpc.miiverse.port}`);
-const gRPCMiiverseClient = createClient(MiiverseServiceDefinition, gRPCMiiverseChannel);
+// Temporarily disabled: Miiverse must not block account cleanup or HTTP startup.
+// const gRPCMiiverseChannel = createChannel(`${config.grpc.miiverse.host}:${config.grpc.miiverse.port}`);
+// const gRPCMiiverseClient = createClient(MiiverseServiceDefinition, gRPCMiiverseChannel);
 
 const PNIDSchema = new Schema<IPNID, PNIDModel, IPNIDMethods>({
 	deleted: {
@@ -315,17 +315,18 @@ PNIDSchema.method('scrub', async function scrub() {
 		}
 	}
 
-	try {
-		await gRPCMiiverseClient.deleteAccountData({
-			pid: this.pid
-		}, {
-			metadata: Metadata({
-				'X-API-Key': config.grpc.miiverse.api_key
-			})
-		});
-	} catch (error) {
-		LOG_ERROR(`ERROR REMOVING ${this.username} MIIVERSE DATA. ${error}`);
-	}
+	// Temporarily disabled: skip Miiverse cleanup until the service is configured.
+	// try {
+	// 	await gRPCMiiverseClient.deleteAccountData({
+	// 		pid: this.pid
+	// 	}, {
+	// 		metadata: Metadata({
+	// 			'X-API-Key': config.grpc.miiverse.api_key
+	// 		})
+	// 	});
+	// } catch (error) {
+	// 	LOG_ERROR(`ERROR REMOVING ${this.username} MIIVERSE DATA. ${error}`);
+	// }
 
 	await this.updateMii({
 		name: 'Default',
